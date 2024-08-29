e293ef281693d3812fcf2fb0b4f35f0bb1cad10c
Siddharth <sengarsiddharth@yahoo.in>
2024-08-29 16:39:34 +0530

    Solved till 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) 
+{
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = string
+    .toLowerCase()
+    .split('')
+    .filter(charac => alphanumerical.includes(charac))
+    .join('');
 
+    let start = 0;
+    let end = cleanString.length - 1;
+
+    while(start <= end)
+    {
+        if(cleanString[start] !== cleanString[end])
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
