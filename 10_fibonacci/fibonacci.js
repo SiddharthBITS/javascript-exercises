b52b9f6e5a9f21874f2cf95a34817938a54b29b1
Siddharth <sengarsiddharth@yahoo.in>
2024-08-29 16:52:53 +0530

    Solved till 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) 
+{
+    let at;
+    if(typeof val !== 'number')
+    {
+        at = parseInt(val);
+    }
+    else
+    {
+        at = val;
+    }
+    if(at < 0)
+    {
+        return 'OOPS';
+    }
+    if(at == 0 || at == 1)
+    {
+        return at;
+    }
+    let minus2 = 0;
+    let minus1 = 1;
+    let fibo = 0;
+    for(let i = 2; i <= at; i++)
+    {
+        fibo = minus1 + minus2;
+        minus2 = minus1;
+        minus1 = fibo;
+    }
+    return fibo;
 };
 
 // Do not edit below this line
